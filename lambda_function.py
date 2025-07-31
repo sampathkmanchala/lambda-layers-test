725c8eab30b17c128a007f450f5edb6b860a26f8
sampathkmanchala <60176602+sampathkmanchala@users.noreply.github.com>
2025-07-31 15:15:38 +0530

    changing url to google.com

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
+
 import requests
 
 def lambda_handler(event, context):
     print(f"Version of requests library: {requests.__version__}")
-    request = requests.get('https://api.github.com/')
+    request = requests.get('https://www.google.com/')
     return {
         'statusCode': request.status_code,
         'body': request.text
