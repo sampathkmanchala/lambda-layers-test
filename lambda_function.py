7b6f5e860aebd63dcdb93596e4ae2ccff88ee506
sampathkmanchala <60176602+sampathkmanchala@users.noreply.github.com>
2025-08-04 19:13:50 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,7 @@ from faker import Faker
 def lambda_handler(event, context):
     print(f"Version of requests library: {requests.__version__}")
     request = requests.get('https://www.google.com/')
+    fake = Faker()
     address = fake.street_address()
     return {
         'statusCode': request.status_code,
