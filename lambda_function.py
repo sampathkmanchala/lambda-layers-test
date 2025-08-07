06e0e85554e257c34e5ae05abb1f75cb26e4777a
sampathkmanchala <60176602+sampathkmanchala@users.noreply.github.com>
2025-08-07 10:40:03 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,16 @@
 
 import requests
 from faker import Faker
+from pyqrcode import create as create_qr_code
 
 def lambda_handler(event, context):
     print(f"Version of requests library: {requests.__version__}")
     request = requests.get('https://www.google.com/')
     fake = Faker()
     address = fake.street_address()
+    qr_code = create_qr_code('"hellow world"', encoding='utf16')
+    print("qr_code:",qr_code)
+    
     return {
         'statusCode': request.status_code,
         'body': {"html":request.text, "address":address}
