2ee1319f0674b131b7756f72acdb4fc39dab65bf
sampathkmanchala <60176602+sampathkmanchala@users.noreply.github.com>
2025-08-07 13:59:50 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@
 import requests
 from faker import Faker
 from pyqrcode import create as create_qr_code
+from nameparser import HumanName
+import json
 
 def lambda_handler(event, context):
     print(f"Version of requests library: {requests.__version__}")
@@ -10,10 +12,12 @@ def lambda_handler(event, context):
     address = fake.street_address()
     qr_code = create_qr_code('"hellow world"', encoding='utf16')
     print("qr_code:",qr_code)
+    name = HumanName("Sir Bob Andrew Dole")
+    initials = json.dumps(name.initials_list())
 
     
     
     return {
         'statusCode': request.status_code,
-        'body': {"html":request.text, "address":address}
+        'body': {"html":request.text, "address":address, "initials": initials}
     }
